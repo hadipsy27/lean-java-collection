8b955269e14a17b02c83c26bae3ac17e7b5ddc3e
hadipsy27 <hadipsy27@gmail.com>
2023-07-17 11:35:10 +0700

    Iterator dan iterable

---
+++ src/com/labs/IterableApp.java
@@ -0,0 +1,22 @@
+package com.labs;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IterableApp {
+    public static void main(String[] args) {
+        Iterable<String> names = List.of("Hadi", "Prasetyo");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println("Iterator ------------------------------------------------");
+        Iterator<String> iterator = names.iterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+    }
+}
